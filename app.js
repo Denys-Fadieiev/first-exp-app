17197a4ae5e4caee09a6b9bdde2b26b5fa16b839
Denys-Fadieiev <dencheek0911@gmail.com>
2023-03-11 14:59:45 +0200

    add

--- app.js
+++ app.js
@@ -1,88 +1,48 @@
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const { format } = require('date-fns');
+
+const { validate, errorHandlers } = require('./middleware');
+const { contactsController } = require('./controllers');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('app )))');
-});
+app.use(express.json());
 
-// CRUD
-app.get('/posts', (req, res) => {});
-app.get('/posts/1', (req, res) => {});
-app.post('/posts', (req, res) => {});
-app.patch('/posts/1', (req, res) => {});
-app.delete('/posts/1', (req, res) => {});
-//----------------------------------------------------------
-const contactsDB = [
-  {
-    id: 0,
-    name: 'Test',
-    telNumber: '+380123456789',
-    birthday: '2000-12-01',
-    isFavourite: false,
+app.get(
+  '/',
+  (req, res, next) => {
+    console.log('handler 1 :>> ');
+    next();
   },
-  {
-    id: 1,
-    name: 'Test1',
-    telNumber: '+380123456788',
-    birthday: format(new Date(), 'Y-MM-dd'),
-    isFavourite: true,
-  },
-];
-class ContactsDB {
-  constructor (arr) {
-    this.contacts = [...arr];
+  (req, res) => {
+    console.log('handler 2 :>> ');
+    res.send('app )))');
   }
+);
 
-  createContact (newContact) {
-    this.contacts.push({ ...newContact, id: uuidv4(), isFavourite: false });
-    return this.contacts[this.contacts.length - 1];
-  }
+// GET /constacts?page=1&results=5
+app.get('/contacts/', contactsController.getContacts);
 
-  getContacts () {
-    return [...this.contacts];
-  }
+// POST /contacts (body)
+app.post(
+  '/contacts',
+  validate.validateContactOnCreate,
+  contactsController.createContact
+);
 
-  getContactById (id) {
-    const foundIndex = this.contacts.findIndex(c => c.id === Number(id));
-    return foundIndex === -1 ? null : this.contacts[foundIndex];
-  }
+// GET /contacts/5
+app.get('/contacts/:id', contactsController.getContactById);
 
-  updateContact (id, values) {
-    const foundContactIndex = this.contacts.findIndex(c => c.id === Number(id));
-    if (foundContactIndex !== -1) {
-      this.contacts[foundContactIndex] = {
-        ...this.contacts[foundContactIndex],
-        ...values,
-      };
-    }
+// PATCH /contacts/5 (body)
+app.patch(
+  '/contacts/:id',
+  validate.validateContactOnUpdate,
+  contactsController.updateContactById
+);
 
-    return foundContactIndex === -1 ? null : this.contacts[foundContactIndex];
-  }
+// реалізувати endpoint для видалення конкретного контакту
+// DELETE /contacts/5
+app.delete('/contacts/:id', contactsController.deleteContactById);
 
-  deleteContact (id) {
-    const foundContactIndex = this.contacts.findIndex(c => c.id === Number(id));
-
-    return foundContactIndex === -1
-      ? null
-      : this.contacts.splice(foundContactIndex, 1);
-  }
-}
-
-const contactsDbInstace = new ContactsDB(contactsDB);
-
-const createdContact = contactsDbInstace.createContact({
-  name: 'Test',
-  telNumber: '+380123456789',
-  birthday: '2000-12-01',
-});
-
-const contacts = contactsDbInstace.getContacts();
-
-console.log('createdContact :>> ', createdContact);
-
-console.log('contacts :>> ', contacts);
+app.use(errorHandlers.validationErrorHandler, errorHandlers.errorHandler);
 
 module.exports = app;
